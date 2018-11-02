ec524d5b3e2e1495692b1a7aa35f1d1c01c37869
Brooke Gill <brooke.gill25@gmail.com>
2018-11-02 18:53:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  const deepChild = document.querySelector('#grand-node div div div div');
+  return deepChild;
+}
